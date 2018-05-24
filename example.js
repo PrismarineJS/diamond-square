f922404e161f342552f6078431586faf5bff00ba
Romain Beaumont <romain.rom1@gmail.com>
2018-05-25 00:08:57 +0200

    convert to standard

--- example.js
+++ example.js
@@ -1,7 +1,7 @@
-var World=require("prismarine-world");
-const Vec3 = require('vec3').Vec3;
+const World = require('prismarine-world')
+const Vec3 = require('vec3').Vec3
 
-var diamondSquare=require("diamond-square")({seed:Math.floor(Math.random()*Math.pow(2, 31))});
-var world=new World(diamondSquare);
+const diamondSquare = require('diamond-square')({seed: Math.floor(Math.random() * Math.pow(2, 31))})
+const world = new World(diamondSquare)
 
-world.getBlock(new Vec3(3,50,3)).then(block => console.log(JSON.stringify(block,null,2)));
+world.getBlock(new Vec3(3, 50, 3)).then(block => console.log(JSON.stringify(block, null, 2)))
